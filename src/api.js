b7cd8c526367fdcde5ea6e3cfda79a036d7b4e8b
geovanejh <geovanehartmann@gmail.com>
2022-08-22 14:29:46 -0300

    fix url pai

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://gerenciador-curriculo-teste.herokuapp.com/",
+  baseURL: "https://gerenciador-curriculo.herokuapp.com/",
 });
 
 export const cepApi = axios.create({
